69e9e9ccbd1b2be2269ec6bbf4bf996765c2b5ef
KC <kcom@KC.local>
2017-02-17 11:31:22 -0500

    added notebooks

--- build_tools/conda_setup.sh
+++ build_tools/conda_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 conda env remove --yes -n freediscovery-env
-conda config --append channels conda-forge
+#conda config --append channels conda-forge
 conda create -n freediscovery-env --yes --file requirements.txt python=3.6
 
 source activate freediscovery-env
